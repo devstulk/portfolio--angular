e9d845796bd45d4b56bcba5b0f658c008a9bcca5
STULK <devstulk@gmail.com>
2024-10-31 21:27:15 -0300

    cria a seção de 'quem sou eu' e faz alguns ajustes a mais

---
+++ src/app/components/link-cta/link-cta.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-link-cta',
+  standalone: true,
+  imports: [],
+  templateUrl: './link-cta.component.html',
+  styleUrl: './link-cta.component.scss'
+})
+export class LinkCtaComponent {
+
+}
